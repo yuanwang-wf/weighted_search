3e749e78d1565265caa44de1b30dafa9fac7a3c1
yuanwang <yuan.wang@workiva.com>
2021-06-28 06:16:03 -0600

    update

--- src/Tree.hs
+++ src/Tree.hs
@@ -1,6 +1,23 @@
 
 module Tree where
 
-data Tree a = Tip a | a :&: [Tree a]
+import Control.Applicative (Alternative, empty, (<|>))
 
-tree = 1 :&: []
+data Tree a = Tip | a :&: [Tree a]
+
+tree :: Tree Int
+tree = 1 :&: [2 :&: [5 :&: [9 :&: []
+                           , 10 :&: []]
+                    , 6 :&: []]
+             , 3 :&: []
+             , 4 :&: [7 :&: [11 :&: []
+                            , 12 :&: []]
+                     , 8 :&: []]]
+
+dfe :: Tree a -> [a]
+dfe Tip = []
+dfe (x :&: xs) = [x] ++ choices dfe xs
+
+choices :: Alternative f => (a -> f b) -> [a] -> f b
+choices f [] = empty
+choices f (x:xs) = f x <|> choices f xs
